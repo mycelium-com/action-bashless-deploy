c754bddff0670759661655b1d3435cf5aec39134
AlexanderPavlenko <AlexanderPavlenko@users.noreply.github.com>
2021-02-05 19:56:09 +0300

    debugs ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ echo -e "${BLUE}Commands:${NORMAL} ${INPUT_COMMAND}"
 
 for host in ${INPUT_HOSTS}; do
   echo -e "${BLUE}Connecting to ${host}...${NORMAL}"
-  sh -c "ssh -q -t -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '${host}' ${INPUT_COMMAND}"
+  sh -c "ssh -v -t -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no '${host}' ${INPUT_COMMAND}"
 done
 
 echo -e "${BLUE}Bashless Deploy Action completed.${NORMAL}"
